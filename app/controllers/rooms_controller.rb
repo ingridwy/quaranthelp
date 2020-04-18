d0bf5ac5771897f0703c2f495c039c3bdde62128
Ingrid Cheng <bluebear589@gmail.com>
2020-04-18 17:48:00 +0100

    fixed bug on room_channel.js

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,10 @@
 class RoomsController < ApplicationController
+def index
+  @rooms = Room.all
+end
+
 def show
-  @room = Room.find(params[:id])
+  @room = Room.includes(messages: :user).find(params[:id])
   @message = Message.new
   end
 
